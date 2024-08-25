7a425fe17d29ca49fd25f61feff2680db5dbd517
Thiago Murilo Diniz <thiagodiniz.info@gmail.com>
2024-08-25 02:57:55 -0400

    applies terraform fmt

--- variables.tf
+++ variables.tf
@@ -1,11 +1,11 @@
 # GENERAL CONFIGS
 
 variable "project_name" {
-  type = string
+  type        = string
   description = "Nome do projeto. Utilizado como prefixo para os recursos criados dentro desse projeto."
 }
 
 variable "aws_region" {
-  type = string
+  type        = string
   description = "Região da AWS onde os recursos serão criados."
 }
